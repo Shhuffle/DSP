b5aaf58df19936361484c7e85c11b8cfca545ef5
Shhuffle <080bei034.safal@pcampus.edu.np>
2025-09-13 22:21:09 +0545

    Added feature up to fft of windowed input discrete signal

---
+++ MFCC/FFT.py
@@ -0,0 +1,90 @@
+'''
+In this code following operations are performed:
+1. Take the input audio using soundfile
+2. PreEmphasize the input discrete signal to boost the higher frequencies energy. Human Speech generally contains low energy
+    high frequencies signal even though  it contain important formants (resonanes that distinguish vowel and consonants).
+    So it is necessary to boost the high frequency signal (1khz-8khz)
+3. Windowing of the preEmphasized signal. Hanning function is used in this code. Windowing allows smooth transition between the
+    frames preventing frequency leakage.
+4. FFt on windowed signal. We generate a frequeny spectrum for each frame using numpy fft library.
+
+
+'''
+
+import soundfile as sf
+import numpy as np 
+import matplotlib.pylab as plt
+
+
+# Load audio
+filename = "my_recording.wav"
+discrete_input, sampling_rate = sf.read(filename)
+
+
+def PreEmphasis(x_n,alpha = 0.97):
+    y = np.zeros(len(x_n))
+    y[0] = x_n[0]
+    for n in range(1,len(x_n)):
+        y[n] = x_n[n] - alpha * x_n[n-1] #first order high pass filter 
+    return y
+
+#emphasized signal 
+x = PreEmphasis(discrete_input[:,0])
+
+#Frame parameters
+frame_length  = 0.025 #25ms
+hop_length = 0.01 #10ms
+hop_size = int(sampling_rate * hop_length)
+frame_size = int(sampling_rate * frame_length)
+total_frames = int((len(x) - frame_size) / hop_size)
+
+def frames(x,start):
+    next_frame = start+hop_size
+    current_frame_val =  x[start:start+frame_size]
+    return next_frame, current_frame_val
+
+
+def Windowing(x,frame_start):
+    next_start ,x_f = frames(x,frame_start)
+    w = np.hanning(len(x_f))
+    windowed_frame = x_f * w
+    return next_start, windowed_frame
+
+#The following function will return a 2D list (row = frame, column = Fourier Transform X(ejw)) which contains the 
+#DTFT of all the frames. This will be later used to generate mel 
+def Wfft(start):
+    X = np.zeros((total_frames,frame_size//2),dtype=complex)
+    
+    for i in range(total_frames):
+        start,windowed_frame= Windowing(x,start)
+
+        X[i,:] = np.fft.fft(windowed_frame)[:frame_size//2] #positive frequencies only
+       
+    return np.abs(X) #We are interesed only in the real part
+
+
+
+if __name__ == "__main__":
+    xf = Wfft(0)
+    fig,axs = plt.subplots(1,2,figsize = (12,6))
+    k = np.arange(frame_size//2)    #up to Nyquist frequency
+    frequency_axis = (k *sampling_rate) / (frame_size*1000) #in Khz
+
+    # Plot the magnitude spectrum
+    frame_number = 200
+    axs[0].plot(frequency_axis, xf[frame_number,:])
+    axs[0].set_title(f"Magnitude Spectrum of frame {frame_number}")
+    axs[0].set_xlabel("Frequency [kHz]")
+    axs[0].set_ylabel("Magnitude")
+    axs[0].grid(True)
+
+    frame_number = 201
+    axs[1].plot(frequency_axis, xf[frame_number,:])  
+    axs[1].set_title(f"Magnitude Spectrum of frame {frame_number}")
+    axs[1].set_xlabel("Frequency [kHz]")
+    axs[1].set_ylabel("Magnitude")
+    axs[1].grid(True)
+    plt.show()
+
+
+    
