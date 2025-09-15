d491ee25f86edda349b72ce3c20c12835dba1848
Shhuffle <080bei034.safal@pcampus.edu.np>
2025-09-15 15:45:26 +0545

    Added a threshold system to not listed label mfccs coefficients  as unkown

--- MFCC/KNNForMFCCs.py
+++ MFCC/KNNForMFCCs.py
@@ -1,4 +1,5 @@
-#In this code KNN (K-Nearest Numbers) algorithm will be implemented from scratch using numpy.
+#In this code KNN (K-Nearest Numbers) algorithm will be implemented on the featured mfccs values computed using DCT.py
+#This program will compare the unknown featured mfccs with the known values and give the nearest known label. 
 import numpy as np 
 import DCT
 
@@ -14,7 +15,7 @@ C/2    ..
 
 #since we have 13 coefficients we need to compute the distance of dimenstion 13
 
-def computeDistance(C,U,kN=4):
+def computeDistance(C,U,kN=4,Threshold = 30):
     total_Known_frames = len(C[:,0])
     dimesion_n_distance = np.zeros((total_Known_frames,2),dtype=object)
     C_features = C[:, 1:].astype(float)  
@@ -25,6 +26,8 @@ def computeDistance(C,U,kN=4):
             partial_distance += (C_features[i,k] - U[k]) ** 2 
         dimesion_n_distance[i,:] = [C[i,0],np.sqrt(partial_distance)]
     sorted_n_dimention_distance = dimesion_n_distance[dimesion_n_distance[:,1].argsort()]
+    if sorted_n_dimention_distance[0,1] > Threshold:
+        return "UNKNOWN"
     K_labels = sorted_n_dimention_distance[0:kN,0] #will return k shortest labels between the unknown and known labels
     K_labels = [str(label) for label in K_labels] #This will explicitly convert the elements to Python native string type form object
     #attribute which will make printing clean.
@@ -33,7 +36,7 @@ def computeDistance(C,U,kN=4):
 
 
 
-#Now implementing majority voting on the k labels
+#Now implementing majority voting on the k labels, to be used when multiple featured mfccs exists for same letter.
 def majorityLabelVoting(k_labels,Total_Labels = 3):
     counter = np.zeros(Total_Labels)
     
@@ -64,7 +67,7 @@ def majorityLabelVoting(k_labels,Total_Labels = 3):
 #NOTE : provide the known frame value C in ascending order i.e 0,1,2....
 #U = DCT.MFCCS.mean(axis=0)
 U = DCT.feature_vector #get the feature vector of my_recording.wav form the dct.py 
-
+print(U)
 '''
 0 - A
 1 - B
@@ -81,7 +84,8 @@ C = np.array([
                 ['i',128.66864724, -97.85098657, 14.86990785, 10.35837987, -18.93308472, 38.76781705, -14.18255467, -13.28664078, 14.40136285, -9.7082583, -2.19200674, 0.65061844, 6.1445766],
                 ['o',113.52880575, -49.47377354, 63.44192718, 28.16339865, -10.79038955, 33.87289352, -36.72913229, -26.10591055, 17.66863745, -6.10231173, -3.6140773, 6.48185176, 17.91188184],
                 ['u',100.62456049, -64.49713876, 33.64326447, 18.52672853, -24.46638633, 40.01955746, 4.50950458, -16.51784523, 1.50821074, -8.5341442, 7.59758049, 13.79179869, 3.05324052],
-                ['kukur',55.13845151, -36.47123441, 17.70765325, 8.5858624, -21.93785234, 21.83142018, -4.14384883, -11.58240279, 11.38908748, -1.65898966, -3.12794806, 4.94856461, 2.06719662]
+                ['kukur',89.51741758, -51.77533918, 42.95886541, 12.28330478, -27.42368507, 36.72793219, -4.11181619, -15.64226847, 9.55486595, -3.78837517, 7.5484735, 12.45134504, 3.84213374]
+
 
               ])
 #parameters
